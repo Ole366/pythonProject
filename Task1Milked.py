0b6c88a95ba1ced51b20678196ffa4a09ed17ed1
Ole366 <you@example.com>
2022-10-27 10:49:58 +0300

    Seminar 9

---
+++ Task1Milked.py
@@ -0,0 +1,6 @@
+# Создайте программу для игры в "Крестики-нолики" при помощи виртуального окружения и PIP
+# Using https://pypi.org/project/tic-tac-toe-miked/
+
+from tic_tac_toe_miked import ttt
+
+ttt.play()
